ca085aa2decc9a067c624356c664823ac5d5b063
Juan Astaiza <juanastaizafuenmayor@gmail.com>
2024-01-25 17:59:37 -0500

    Some errors solved

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package moviesProyectAPI
 
 import (
 	"fmt"
+	"github.com/JuanEstebanAstaiza/MovieProyectAPI/controllers"
 	"log"
 	"net/http"
 
+	_ "github.com/JuanEstebanAstaiza/MovieProyectAPI/controllers"
 	"github.com/gorilla/mux"
 )
 
@@ -12,39 +14,15 @@ func main() {
 	router := mux.NewRouter()
 
 	// Configurar rutas
-	router.HandleFunc("/api/movie/{movie_id}", getMovieDetails).Methods("GET")
-	router.HandleFunc("/api/most-viewed", getMostViewedMovies).Methods("GET")
-	router.HandleFunc("/api/comment", addComment).Methods("POST")
-	router.HandleFunc("/api/comment", deleteComment).Methods("DELETE")
-	router.HandleFunc("/api/comment", editComment).Methods("PUT")
-	router.HandleFunc("/api/user/{user_id}", modifyUserInfo).Methods("PUT")
+	router.HandleFunc("/api/movie/{movie_id}", controllers.GetMovieDetails()).Methods("GET")
+	router.HandleFunc("/api/most-viewed", controllers.GetMostViewedMovies()).Methods("GET")
+	router.HandleFunc("/api/comment", controllers.AddComment()).Methods("POST")
+	router.HandleFunc("/api/comment", controllers.DeleteComment()).Methods("DELETE")
+	router.HandleFunc("/api/comment", controllers.EditComment()).Methods("PUT")
+	router.HandleFunc("/api/user/{user_id}", controllers.ModifyUserInfo).Methods("PUT")
 
 	// Iniciar el servidor
 	port := ":8080"
 	fmt.Println("Servidor escuchando en el puerto", port)
 	log.Fatal(http.ListenAndServe(port, router))
-}
-
-func getMovieDetails(w http.ResponseWriter, r *http.Request) {
-	// Implementar lógica para obtener detalles de la película
-}
-
-func getMostViewedMovies(w http.ResponseWriter, r *http.Request) {
-	// Implementar lógica para obtener las n películas más visualizadas
-}
-
-func addComment(w http.ResponseWriter, r *http.Request) {
-	// Implementar lógica para agregar comentarios
-}
-
-func deleteComment(w http.ResponseWriter, r *http.Request) {
-	// Implementar lógica para eliminar comentarios
-}
-
-func editComment(w http.ResponseWriter, r *http.Request) {
-	// Implementar lógica para editar comentarios
-}
-
-func modifyUserInfo(w http.ResponseWriter, r *http.Request) {
-	// Implementar lógica para modificar información del usuario
 }
---
+++ sql/create_tables.sql
@@ -0,0 +1,32 @@
+-- Crear la tabla de usuarios
+CREATE TABLE users (
+                       id INT PRIMARY KEY,
+                       nickname VARCHAR(50) NOT NULL,
+                       email VARCHAR(100) NOT NULL,
+                       password VARCHAR(255) NOT NULL
+
+);
+
+-- Crear la tabla de películas
+CREATE TABLE movies (
+                        id INT PRIMARY KEY,
+                        api_id INT NOT NULL,
+                        visualizations INT DEFAULT 0, -- default value = 0
+                        title VARCHAR(255) NOT NULL,
+                        overview TEXT,
+                        release_date DATE,
+                        original_language VARCHAR(10)
+
+);
+
+-- Crear la tabla de comentarios
+CREATE TABLE comments (
+                          id INT PRIMARY KEY,
+                          user_id INT,
+                          movie_id INT,
+                          comment_text TEXT,
+                          timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                          FOREIGN KEY (user_id) REFERENCES users(id),
+                          FOREIGN KEY (movie_id) REFERENCES movies(id)
+
+);
